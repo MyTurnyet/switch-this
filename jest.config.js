edbb03e25c66df3011c9554be233077527d6972c
paige.watson <paige.watson@salesforce.com>
2025-04-23 09:35:31 -0700

    🔧 switch to SWC - Remove Babel and use SWC for both Next.js and testing

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,12 @@
-module.exports = {
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './',
+});
+
+// Add any custom config to be passed to Jest
+const customJestConfig = {
   testEnvironment: 'jsdom',
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
@@ -6,12 +14,12 @@ module.exports = {
     '^next/font/google$': '<rootDir>/__mocks__/next/font/google.js',
   },
   transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+    '^.+\\.(t|j)sx?$': ['@swc/jest'],
   },
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testPathIgnorePatterns: ['<rootDir>/node_modules/'],
-  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json'],
-  transformIgnorePatterns: [
-    '/node_modules/(?!(next|@babel/runtime)/)',
-  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
 };
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(customJestConfig);
