2a16fa727a00703ad3fe59cf2379fa48c68bda93
paige.watson <paige.watson@salesforce.com>
2025-04-23 09:30:59 -0700

    🔧 switch to SWC - Update configuration to use SWC for Next.js build and Babel only for tests

--- babel.config.js
+++ babel.config.js
@@ -10,5 +10,21 @@ module.exports = {
     ['@babel/plugin-transform-runtime', {
       regenerator: true
     }]
-  ]
+  ],
+  env: {
+    test: {
+      presets: [
+        ['@babel/preset-env', { targets: { node: 'current' } }],
+        '@babel/preset-typescript',
+        ['@babel/preset-react', { runtime: 'automatic' }],
+      ],
+      plugins: [
+        '@babel/plugin-transform-typescript',
+        '@babel/plugin-syntax-typescript',
+        ['@babel/plugin-transform-runtime', {
+          regenerator: true
+        }]
+      ]
+    }
+  }
 }; 
--- next.config.js
+++ next.config.js
@@ -1,16 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ['next'],
   experimental: {
     appDir: true,
   },
   typescript: {
     ignoreBuildErrors: false,
   },
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false, path: false };
-    return config;
-  },
+  swcMinify: true,
 };
 
 module.exports = nextConfig; 
