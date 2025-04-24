1012ac55d8d9434a9e78e808f2a6f48a0753b373
paige.watson <paige.watson@salesforce.com>
2025-04-24 10:17:07 -0700

    :wrench: config: update Jest and TypeScript configurations

--- jest.config.js
+++ jest.config.js
@@ -12,10 +12,23 @@ const customJestConfig = {
     '^@shared/test-utils$': '<rootDir>/src/app/shared/test-utils',
     '^@shared/test-utils/(.*)$': '<rootDir>/src/app/shared/test-utils/$1',
     '^@shared/(.*)$': '<rootDir>/src/app/shared/$1',
+    '^@app/(.*)$': '<rootDir>/src/app/$1',
+    '^@components/(.*)$': '<rootDir>/src/components/$1',
+    '^@services/(.*)$': '<rootDir>/src/app/services/$1',
+    '^@types/(.*)$': '<rootDir>/src/types/$1',
   },
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': ['@swc/jest'],
   },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/*.stories.{js,jsx,ts,tsx}',
+    '!src/**/index.{js,jsx,ts,tsx}',
+  ],
 };
 
 export default createJestConfig(customJestConfig); 
