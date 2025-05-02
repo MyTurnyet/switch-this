a45f0f5f5c6b49b4816b45a19821a257bbf9c4e0
paige.watson <paige.watson@salesforce.com>
2025-05-02 07:40:15 -0700

    [B] Display locations in Layout State and Industries pages even without industries

--- src/app/layout-state/utils/groupIndustries.ts
+++ src/app/layout-state/utils/groupIndustries.ts
@@ -32,25 +32,40 @@ export function groupIndustriesByLocationAndBlock(
 ): GroupedIndustries {
   const locationMap = new Map(locations.map(loc => [loc._id, loc]));
   
-  return industries.reduce((grouped, industry) => {
+  // Start with an empty result object
+  const result: GroupedIndustries = {};
+  
+  // First, make sure every location has an entry
+  locations.forEach(location => {
+    if (!result[location._id]) {
+      result[location._id] = {
+        locationName: location.stationName,
+        blocks: {}
+      };
+    }
+  });
+  
+  // Then add all industries to their respective locations and blocks
+  industries.forEach(industry => {
     const location = locationMap.get(industry.locationId);
-    if (!location) return grouped;
+    if (!location) return;
 
-    if (!grouped[industry.locationId]) {
-      grouped[industry.locationId] = {
+    if (!result[industry.locationId]) {
+      result[industry.locationId] = {
         locationName: location.stationName,
         blocks: {}
       };
     }
 
     const block = location.block;
-    if (!grouped[industry.locationId].blocks[block]) {
-      grouped[industry.locationId].blocks[block] = [];
+    if (!result[industry.locationId].blocks[block]) {
+      result[industry.locationId].blocks[block] = [];
     }
 
-    grouped[industry.locationId].blocks[block].push(industry);
-    return grouped;
-  }, {} as GroupedIndustries);
+    result[industry.locationId].blocks[block].push(industry);
+  });
+  
+  return result;
 } 
 
 export function groupIndustriesByBlockAndLocation(
@@ -59,31 +74,56 @@ export function groupIndustriesByBlockAndLocation(
 ): GroupedByBlockAndLocation {
   const locationMap = new Map(locations.map(loc => [loc._id, loc]));
   
-  return industries.reduce((grouped, industry) => {
-    const location = locationMap.get(industry.locationId);
-    if (!location) return grouped;
-    
+  // Start with an empty result object
+  const result: GroupedByBlockAndLocation = {};
+  
+  // First, make sure every location has an entry
+  locations.forEach(location => {
     const blockName = location.block;
     
     // Create block entry if it doesn't exist
-    if (!grouped[blockName]) {
-      grouped[blockName] = {
+    if (!result[blockName]) {
+      result[blockName] = {
         blockName,
         locations: {}
       };
     }
     
     // Create location entry if it doesn't exist
-    if (!grouped[blockName].locations[location._id]) {
-      grouped[blockName].locations[location._id] = {
+    if (!result[blockName].locations[location._id]) {
+      result[blockName].locations[location._id] = {
+        locationName: location.stationName,
+        industries: []
+      };
+    }
+  });
+  
+  // Then add all industries to their respective locations
+  industries.forEach(industry => {
+    const location = locationMap.get(industry.locationId);
+    if (!location) return;
+    
+    const blockName = location.block;
+    
+    // This should already exist from the initialization above,
+    // but just in case, create it if it doesn't
+    if (!result[blockName]) {
+      result[blockName] = {
+        blockName,
+        locations: {}
+      };
+    }
+    
+    if (!result[blockName].locations[location._id]) {
+      result[blockName].locations[location._id] = {
         locationName: location.stationName,
         industries: []
       };
     }
     
     // Add the industry to the location
-    grouped[blockName].locations[location._id].industries.push(industry);
-    
-    return grouped;
-  }, {} as GroupedByBlockAndLocation);
+    result[blockName].locations[location._id].industries.push(industry);
+  });
+  
+  return result;
 } 
